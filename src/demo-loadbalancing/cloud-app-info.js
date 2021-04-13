0f1b7924fba04e2295938ba9cc4da287abad627e
EricGhn <eric.1ghn@gmail.com>
2021-04-13 19:08:29 +0400

    Initial commit

---
+++ src/demo-loadbalancing/cloud-app-info.js
@@ -0,0 +1,10 @@
+﻿
+var AppInfo = {
+//	Wss: true,
+    AppId: "<no-app-id>",
+    AppVersion: "1.0",
+//	MasterServer: "localhost:9090"
+//  NameServer: "ws://localhost:9093"
+//  FbAppId: "you fb app id", 
+}
+
